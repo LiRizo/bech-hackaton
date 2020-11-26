abdb432d89798cabb16c77a6511b767ef34e7d52
Katherine Castillo <csk.kath@gmail.com>
2020-11-26 03:35:19 -0300

    Faq component structure and new request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NewRequestComponent } from './components/new-request/new-request.component';
 import { TrackingNumberComponent } from './components/tracking-number/tracking-number.component';
+import { FrequentQuestionsAllComponent } from './components/frequent-questions-all/frequent-questions-all.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent },
   { path: 'nuevasolicitud', component: NewRequestComponent },
-  { path: 'seguimiento', component: TrackingNumberComponent }
+  { path: 'seguimiento', component: TrackingNumberComponent },
+  { path: 'preguntasfrecuentesall', component: FrequentQuestionsAllComponent }
 ];
 
 @NgModule({
---
+++ src/app/components/frequent-questions-all/frequent-questions-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frequent-questions-all',
+  templateUrl: './frequent-questions-all.component.html',
+  styleUrls: ['./frequent-questions-all.component.css']
+})
+export class FrequentQuestionsAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
