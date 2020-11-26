9fdc586c1c306498f39b5808d351d4006167dfff
Katherine Castillo <csk.kath@gmail.com>
2020-11-26 11:41:10 -0300

    structure faq all

--- src/app/components/frequent-questions-all/frequent-questions-all.component.ts
+++ src/app/components/frequent-questions-all/frequent-questions-all.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frequent-questions-all.component.css']
 })
 export class FrequentQuestionsAllComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
